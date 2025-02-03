9f490279de91ace1e2b85d7c20aa396cc4ffb8e0
daniellee6925 <147388870+daniellee6925@users.noreply.github.com>
2025-02-03 11:26:00 +0900

    add feedforward

--- model.py
+++ model.py
@@ -186,6 +186,30 @@ class SelfAttention(nn.Module):
         return self.wo(output)
 
 
+class FeedForward(nn.Module):
+    def __init__(self, args: ModelArgs):
+        super.__init__()
+        hidden_dim = 4 * args.dim
+        hidden_dim = int(2 * hidden_dim / 3)
+        if args.ffn_dim_multiplier is not None:
+            hidden_dim = int(args.ffn_dim_multiplier * hidden_dim)
+        # round the hidden_dim to the nearest multiplier of the multiple of parameter
+        hidden_dim = args.multiple_of * (
+            (hidden_dim + args.multiple_of - 1) // args.multiple_of
+        )
+
+        self.w1 = nn.Linear(args.dim, hidden_dim, bias=False)
+        self.w2 = nn.Linear(hidden_dim, args.dim, bias=False)
+        self.w3 = nn.Linear(args.dim, hidden_dim, bias=False)
+
+    def forward(self, x: torch.Tensor):
+        swish = F.silu(self.w1(x))
+        x_V = self.w3(x)
+        x = swish * x_V
+        x = self.w2(x)
+        return x
+
+
 def pre_compute_theta_pos_frequencies(
     head_dim: int, seq_len: int, device: str, theta: float = 10000
 ):
