4f51ba7267ecbbe2b70907d07d72d0b0f58701cc
daniellee6925 <147388870+daniellee6925@users.noreply.github.com>
2025-02-09 22:38:04 -0800

    finish loading model parameters

--- inference.py
+++ inference.py
@@ -19,7 +19,65 @@ class LLaMA:
         self.tokenzier = tokenzier
         self.args = ModelArgs
 
-    @staticmethod 
-    def build(checkpoints_dir: str, tokenizer_path: str, load_model: bool, max_seq_len: int, device: str):
-        
-        
+    @staticmethod
+    def build(
+        checkpoints_dir: str,
+        tokenizer_path: str,
+        load_model: bool,
+        max_seq_len: int,
+        max_batch_size: int,
+        device: str,
+    ):
+        prev_time = time.time()
+        if load_model:
+            checkpoints = sorted(Path(checkpoints_dir).glob("*.pth"))
+            assert len(checkpoints) > 0, "No checkpoint files found"
+            chk_pth = checkpoints[0]
+            print(f"Loading Check Point {chk_pth}")
+            checkpoint = torch.load(chk_pth, map_location="cpu")
+            print(f"Loaded Check Point in{(time.time() - prev_time):.2f}s")
+            prev_time = time.time()
+        with open(Path(checkpoints_dir) / "params.json", "r") as f:
+            params = json.loads(f.read())
+        model_args: ModelArgs = ModelArgs(
+            max_seq_len=max_seq_len,
+            max_batch_size=max_batch_size,
+            device=device,
+            **params,
+        )
+
+        tokenizer = SentencePieceProcessor()
+        tokenizer.load(tokenizer_path)
+        model_args.vocab_size = tokenizer.vocab_size()
+
+        if device == "cuda":
+            torch.set_default_tensor_type(torch.cuda.HalfTensor)
+        else:
+            torch.set_default_tensor_type(torch.BFloat16Tensor)
+
+        model = Transformer(model_args).to(device)
+
+        if load_model:
+            del checkpoint["rope.freqs"]
+            model.load_state_dict(checkpoint, strict=True)
+            print(f"Loaded state dict in {(time.time() - prev_time):.2f}s")
+
+        return LLaMA(model, tokenizer, model_args)
+
+
+if __name__ == "__main__":
+    torch.manual_seed(0)
+
+    allow_cuda = False
+    device = "cuda" if torch.cuda.is_available() and allow_cuda else "cpu"
+
+    model = LLaMA.build(
+        checkpoints_dir="llama-2-7b",
+        tokenizer_path="tokenizer.model",
+        load_model=True,
+        max_seq_len=1024,
+        max_batch_size=3,
+        device=device,
+    )
+
+    print("all ok")
