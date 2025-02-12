7eff72adc0955a3168ef030245f237c455a809b3
daniellee6925 <147388870+daniellee6925@users.noreply.github.com>
2025-02-11 17:30:22 -0800

    finish top p sampling

--- inference.py
+++ inference.py
@@ -148,6 +148,8 @@ class LLaMA:
         probs_sort[mask] = 0.0
         probs_sort.div_(probs_sort.sum(dim=-1, keepdim=True))
         next_token = torch.multinomial(probs_sort, num_samples=1)
+        next_token = torch.gather(probs_idx, -1, next_token)
+        return next_token
 
 
 if __name__ == "__main__":
